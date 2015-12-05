7ab3ecb04e14ed1d5598972e59be857d681420b5
macBasement <michaelcoca@gmail.com>
2015-12-04 22:40:07 -0600

    c

--- ch7-ex3p2/Form1.Designer.cs
+++ ch7-ex3p2/Form1.Designer.cs
@@ -32,21 +32,25 @@
             this.goHere = new System.Windows.Forms.Button();
             this.exits = new System.Windows.Forms.ComboBox();
             this.goThroughTheDoor = new System.Windows.Forms.Button();
+            this.check = new System.Windows.Forms.Button();
+            this.hide = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // description
             // 
-            this.description.Location = new System.Drawing.Point(12, 12);
+            this.description.Location = new System.Drawing.Point(9, 10);
+            this.description.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.description.Multiline = true;
             this.description.Name = "description";
-            this.description.Size = new System.Drawing.Size(363, 134);
+            this.description.Size = new System.Drawing.Size(273, 110);
             this.description.TabIndex = 0;
             // 
             // goHere
             // 
-            this.goHere.Location = new System.Drawing.Point(12, 169);
+            this.goHere.Location = new System.Drawing.Point(9, 137);
+            this.goHere.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.goHere.Name = "goHere";
-            this.goHere.Size = new System.Drawing.Size(75, 23);
+            this.goHere.Size = new System.Drawing.Size(56, 19);
             this.goHere.TabIndex = 1;
             this.goHere.Text = "Go here:";
             this.goHere.UseVisualStyleBackColor = true;
@@ -56,30 +60,55 @@
             // 
             this.exits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.exits.FormattingEnabled = true;
-            this.exits.Location = new System.Drawing.Point(93, 168);
+            this.exits.Location = new System.Drawing.Point(70, 136);
+            this.exits.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.exits.Name = "exits";
-            this.exits.Size = new System.Drawing.Size(282, 24);
+            this.exits.Size = new System.Drawing.Size(212, 21);
             this.exits.TabIndex = 2;
             // 
             // goThroughTheDoor
             // 
-            this.goThroughTheDoor.Location = new System.Drawing.Point(12, 218);
+            this.goThroughTheDoor.Location = new System.Drawing.Point(9, 177);
+            this.goThroughTheDoor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.goThroughTheDoor.Name = "goThroughTheDoor";
-            this.goThroughTheDoor.Size = new System.Drawing.Size(363, 23);
+            this.goThroughTheDoor.Size = new System.Drawing.Size(272, 29);
             this.goThroughTheDoor.TabIndex = 3;
             this.goThroughTheDoor.Text = "Go through the door";
             this.goThroughTheDoor.UseVisualStyleBackColor = true;
             this.goThroughTheDoor.Click += new System.EventHandler(this.goThroughTheDoor_Click);
             // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(9, 211);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(272, 29);
+            this.check.TabIndex = 4;
+            this.check.Text = "button1";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // hide
+            // 
+            this.hide.Location = new System.Drawing.Point(9, 246);
+            this.hide.Name = "hide";
+            this.hide.Size = new System.Drawing.Size(272, 31);
+            this.hide.TabIndex = 5;
+            this.hide.Text = "Hide!";
+            this.hide.UseVisualStyleBackColor = true;
+            this.hide.Click += new System.EventHandler(this.hide_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(387, 253);
+            this.ClientSize = new System.Drawing.Size(290, 293);
+            this.Controls.Add(this.hide);
+            this.Controls.Add(this.check);
             this.Controls.Add(this.goThroughTheDoor);
             this.Controls.Add(this.exits);
             this.Controls.Add(this.goHere);
             this.Controls.Add(this.description);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Explore the House";
             this.ResumeLayout(false);
@@ -93,6 +122,8 @@
         private System.Windows.Forms.Button goHere;
         private System.Windows.Forms.ComboBox exits;
         private System.Windows.Forms.Button goThroughTheDoor;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Button hide;
     }
 }
 
--- ch7-ex3p2/Form1.cs
+++ ch7-ex3p2/Form1.cs
@@ -6,6 +6,20 @@ namespace ch7_ex3p2
     public partial class Form1 : Form
     {
         private Location currentLocation;
+        OutsideWithDoor backYard = new OutsideWithDoor("Back Yard", true, "a screen door");
+        OutsideWithHidingPlace garden = new OutsideWithHidingPlace("Garden", false);
+        OutsideWithDoor frontYard = new OutsideWithDoor("Front Yard", false, "an oak door with a brass knob");
+        RoomWithDoor livingRoom = new RoomWithDoor("Living Room", "an antique carpet", "an oak door with a brass knob");
+        Room diningRoom = new Room("Dining Room", "a crystal chandelier");
+        RoomWithDoor kitchen = new RoomWithDoor("Kitchen", "stainless steel appliances", "a screen door");
+        Room stairs = new Room("Stairs", "a wooden banister");
+        RoomWithHidingPlace upstairsHallway = new RoomWithHidingPlace("Upstairs Hallway", "a picture of a dog");
+        RoomWithHidingPlace masterBedroom = new RoomWithHidingPlace("Master Bedroom", "a large bed");
+        RoomWithHidingPlace secondBedroom = new RoomWithHidingPlace("Second Bedroom", "a small bed");
+        RoomWithHidingPlace bathroom = new RoomWithHidingPlace("Bathroom", "a sink and a toilet");
+        OutsideWithHidingPlace driveway = new OutsideWithHidingPlace("Driveway", false);
+        Opponent opponent;
+
 
         public Form1()
         {
@@ -15,25 +29,32 @@ namespace ch7_ex3p2
         
         public void CreateObjects()
         {
-            OutsideWithDoor backYard = new OutsideWithDoor("Back Yard", true, "a screen door");
-            Outside garden = new Outside("Garden", false);
-            OutsideWithDoor frontYard = new OutsideWithDoor("Front Yard", false , "an oak door with a brass knob");
-            RoomWithDoor livingRoom = new RoomWithDoor("Living Room", "an antique carpet", "an oak door with a brass knob");
-            Room diningRoom = new Room("Dining Room", "a crystal chandelier");
-            RoomWithDoor kitchen = new RoomWithDoor("Kitchen", "stainless steel appliances", "a screen door");
 
             backYard.Exits = new Location[] { kitchen, garden };
             garden.Exits = new Location[] { backYard, frontYard };
             frontYard.Exits = new Location[] { garden, livingRoom };
             livingRoom.Exits = new Location[] { frontYard, diningRoom };
-            livingRoom.DoorLocation = frontYard;
-            frontYard.DoorLocation = livingRoom;
             diningRoom.Exits = new Location[] { livingRoom, kitchen };
             kitchen.Exits = new Location[] { diningRoom, backYard };
+            stairs.Exits = new Location[] { livingRoom, upstairsHallway };
+            upstairsHallway.Exits = new Location[] { masterBedroom, secondBedroom, bathroom };
+
+            livingRoom.DoorLocation = frontYard;
+            frontYard.DoorLocation = livingRoom;
             kitchen.DoorLocation = backYard;
             backYard.DoorLocation = kitchen;
 
-            MoveToANewLocation(backYard);
+            upstairsHallway.HidingPlaceName = "the closet";
+            masterBedroom.HidingPlaceName = "under the bed";
+            secondBedroom.HidingPlaceName = "under the bed";
+            bathroom.HidingPlaceName = "the shower";
+            driveway.HidingPlaceName = "the garage";
+            garden.HidingPlaceName = "the shed";
+
+            //MoveToANewLocation(backYard);
+            goHere.Visible = false;
+            exits.Visible = false;
+            goThroughTheDoor.Visible = false;
         }
 
         private void MoveToANewLocation(Location newLocation)
@@ -64,6 +85,16 @@ namespace ch7_ex3p2
                 IHasExteriorDoor withDoor = currentLocation as IHasExteriorDoor;
                 MoveToANewLocation(withDoor.DoorLocation);
             }
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hide_Click(object sender, EventArgs e)
+        {
+            opponent = new Opponent(frontYard);
         }
     }
 }
